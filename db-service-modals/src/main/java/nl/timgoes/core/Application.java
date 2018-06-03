0f883989acb347b65f794b8abc6428f4a567e3a5
Tim Goes <Tim_Goes4@hotmail.nl>
2018-06-03 18:13:10 +0200

    DB-Service-modals is now a small usable jar

---
+++ db-service-modals/src/main/java/nl/timgoes/core/Application.java
@@ -0,0 +1,8 @@
+package nl.timgoes.core;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("This is a small JPA Entity jar, us it as that!");
+    }
+}
